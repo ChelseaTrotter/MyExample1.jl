663f2ccf86cebad2edfcc01d1b25ae4b22a6ee96
ChelseaTrotter <chelsea.xhu@gmail.com>
2020-02-17 13:59:55 -0600

    add testing packages
    adding safetestsets.jl to the project and modified project.toml

---
+++ test/my_f_tests.jl
@@ -0,0 +1,9 @@
+using MyExample1
+using Test
+
+@testset "MyExample1.jl" begin
+    # Write your own tests here.
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
+    @test my_f(1,3) == 11
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,4 @@
-using MyExample1
-using Test
 
+using SafeTestsets
 
-
-
-@testset "MyExample1.jl" begin
-    # Write your own tests here.
-    @test my_f(2,1) == 7
-    @test my_f(2,3) == 13
-    @test my_f(1,3) == 11
-end
+@safetestset "My_f tests " begin include("my_f_tests.jl") end
